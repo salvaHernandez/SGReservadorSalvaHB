96195b639811f395e6c572ce9ffdef755301d72a
mariogmez <mariogomex199921@gmail.com>
2022-03-18 10:33:45 +0100

    CMT 10:33 18/03

--- SGReservadorSalvaHB/FormPrincipal.Designer.cs
+++ SGReservadorSalvaHB/FormPrincipal.Designer.cs
@@ -48,7 +48,7 @@
             this.tslSalir});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.Size = new System.Drawing.Size(1172, 28);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "mnuStrip";
             // 
@@ -96,7 +96,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1172, 802);
             this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.IsMdiContainer = true;
