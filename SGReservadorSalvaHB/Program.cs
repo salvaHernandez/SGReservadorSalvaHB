74f11fd04cc5945559e7b1b7e6e830b55714ca2d
mariogmez <mariogomex199921@gmail.com>
2022-03-18 09:48:45 +0100

    CMT 9:48 18/03

--- SGReservadorSalvaHB/Program.cs
+++ SGReservadorSalvaHB/Program.cs
@@ -17,6 +17,7 @@ namespace SGReservadorSalvaHB
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Console.WriteLine("The password = ");
         }
     }
 }
